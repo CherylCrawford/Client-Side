514332b70c9c50c18aa1d38139b05d9900811108
CherylCrawford <armande38@gmail.com>
2018-09-12 00:28:02 -0500

    add comments

--- HW02/Scripts/temperature.ts
+++ HW02/Scripts/temperature.ts
@@ -10,50 +10,69 @@
 // For: Web Client-Side Programming - HW02                //
 // /////////////////////////////////////////////////////////
 
+// class temperature provides method to get Fahrenheit and display Celsius
 class Temperature {
-    Fahrenheit: number;
-    Celsius: number;
-    static readonly min = -100;
-    static readonly max = +212;
-    static readonly promptStr = "Enter Fahrenheit temperature";
-    static readonly offset = 32.0;
-    static readonly multiplier = 5.0;
-    static readonly divisor = 9.0;
+    Fahrenheit: number; // Fahrenheit temperature internal storage
+    Celsius: number; // Celsius temperature internal storage
 
+    // bounds check and input prompt
+    static readonly min = -100; // minimum value
+    static readonly max = +212; // maximum value
+    static readonly promptStr = "Enter Fahrenheit temperature";
+
+    // formula is Celsius = (Fahrenheit - Offset) * multiplier / divisor
+    static readonly offset = 32.0; // difference between Fahrenheit and Celsius
+    static readonly multiplier = 5.0; // multiplier to convert Fahrenheit to Celsius
+    static readonly divisor = 9.0; // divisor calculates final conversion from Fahrenheit to Celsius
+
+    // initialize properties
     constructor() {
         this.Fahrenheit = 0;
         this.Celsius = 0;
     }
 
+    // method getFahrenheit inputs Fahrenheit and displays Celsius
     getFahrenheit() {
         var txt;
         var fah;
+
         for (; ;) {
             fah = prompt(Temperature.promptStr);
 
+            // test for null input
             if (fah == null || fah == "") {
                 txt = "User cancelled the prompt";
                 break;
-            } else {
+            } else { // evaluate user input
                 this.Fahrenheit = Number(fah);
 
+                // bounds check
                 if ((Temperature.min <= this.Fahrenheit) && (this.Fahrenheit <= Temperature.max)) {
+                    // calculate Celsius
                     this.Celsius = (this.Fahrenheit - Temperature.offset) * Temperature.multiplier / Temperature.divisor;
+
+                    // display text
                     txt = "Fahrenheit temperature is " + this.Fahrenheit.toString(10) + "\nCelsius temperature is " + this.Celsius.toFixed(1);
                     break;
-                } else {
+                } else { // bounds error
                     txt = "You entered " + fah + ".\nEntry must range from " + Temperature.min + " to " + Temperature.max + ".";
                     alert(txt);
                 }
             }
         }
+        // display response
         alert(txt);
+
+        // return response
         document.getElementById("demo").innerHTML = txt;
     }
 }
 
+
+// construct temperature
 let temperature = new Temperature();
 
+// define button
 let button = document.createElement('button');
 button.textContent = "Convert Temperature";
 button.onclick = function () {
